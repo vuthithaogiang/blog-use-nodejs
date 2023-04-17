e29ea03b3831fab104dcdc2dd2f468376d67cfcf
giang <vuthithaogiang050400@gmail.com>
2023-04-17 16:20:14 +0700

    Add Create Product

--- src/app/controllers/ProductController/index.js
+++ src/app/controllers/ProductController/index.js
@@ -7,12 +7,28 @@ class ProductController {
     show(rep, res, next) {
         MyModel.findOne({ slug: rep.params.slug })
             .then((product) => {
-                res.render('courses/show', {
+                res.render('products/show', {
                     product: mongooseToObject(product),
                 });
             })
 
             .catch(next);
+    }
+
+    //GET: products/create
+    create(rep, res, next) {
+        res.render('products/create');
+    }
+
+    //POST: products/store
+    store(rep, res, next) {
+        const formData = rep.body;
+        formData.image = `https://img.youtube.com/vi/${rep.body.videoId}/sddefault.jpg`;
+        const product = new MyModel(formData);
+        product
+            .save()
+            .then(() => res.redirect('/'))
+            .catch((error) => {});
     }
 }
 
--- src/routes/product.js
+++ src/routes/product.js
@@ -3,6 +3,8 @@ import productController from '../app/controllers/ProductController/index.js';
 
 const routerDetails = express.Router();
 
+routerDetails.get('/create', productController.create);
+routerDetails.post('/store', productController.store);
 routerDetails.get('/:slug', productController.show);
 
 export default routerDetails;
